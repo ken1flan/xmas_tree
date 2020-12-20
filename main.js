26f07ac15ef6f08a9f3300f1feceb87520634c04
ken1flan <ken1flan@gmail.com>
2020-12-20 10:24:07 +0900

    Add png

--- main.js
+++ main.js
@@ -7,15 +7,15 @@ function initialize() {
 function draw() {
   var canvas = document.getElementById('tutorial');
   var ctx = canvas.getContext('2d');
-  // file://だとcanvasが汚染扱いになってしまうので、コメントアウト。
-  // どこかサーバに置けば大丈夫とのこと。
-  // var img = new Image();
-  // img.src = 'images/really.png';
-  // img.onload = function () {
-  //   ctx.drawImage(img, 0, 0, 900, 900);
-  //   ctx.font = '72px serif';
-  //   ctx.fillText('まぢ？', 50, 100);
-  // };
+  file://だとcanvasが汚染扱いになってしまうので、コメントアウト。
+  どこかサーバに置けば大丈夫とのこと。
+  var img = new Image();
+  img.src = 'really.png';
+  img.onload = function () {
+    ctx.drawImage(img, 0, 0, 900, 900);
+    ctx.font = '72px serif';
+    ctx.fillText('まぢ？', 50, 100);
+  };
   ctx.fillStyle = 'white';
   ctx.fillRect(0, 0, 500, 500);
   var text = document.getElementById('inputText').value;
